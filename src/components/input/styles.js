c7afbd78ffe9ea6c792069cf13b62f93653109ee
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-02-20 20:09:55 -0300

    Design Calculadora

--- src/components/input/styles.js
+++ src/components/input/styles.js
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #AAFFAA;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+  font-family: 'Roboto', sans-serif;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #65789d;
+    border: 0;
+    text-align: end;
+    padding: 0 10px;
+    font-size: 24px;
+    color: #FFF
+  }
+`
